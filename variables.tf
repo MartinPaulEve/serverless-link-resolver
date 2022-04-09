428c4aaf3cca37380f98914a128815fe3e1e0ab1
Martin Paul Eve <martin@martineve.com>
2022-04-09 18:21:28 +0100

    Rework variables

--- variables.tf
+++ variables.tf
@@ -1,13 +1,3 @@
-variable "key_name" {
-  type    = string
-  default = "wirevpn"
-}
-
-variable "mobile" {
-  type    = bool
-  default = false
-}
-
 variable "availability_zones" {
   type    = list(string)
   default = ["us-east-1a", "us-east-1b", "us-east-1c"]
